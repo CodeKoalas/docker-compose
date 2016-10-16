8c735c3e6039f13dfa15c52f6bb4e854354df4c5
Kerry Knopp <kknopp@Kerrys-iMac.local>
2016-10-16 15:33:38 -0500

    updating syntax and references

---
+++ updatedev.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd $DIR && git pull origin master
+
+docker ps -a | grep -v STATUS | awk '{print $2}' | sort | uniq | xargs -n 1 docker pull
