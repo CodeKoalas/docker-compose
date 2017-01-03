ea9b9a4bd9597f8e74ddc2e5c731e31299b456db
Kerry Knopp <kerry@codekoalas.com>
2017-01-03 17:00:45 -0600

    adding xdebug to drupal templates and adding features to setup script

---
+++ addloopback.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# https://gist.github.com/krknopp/80720dc28e13a95f1704d33a5dbb174f
+
+echo "<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+  <dict>
+      <key>Label</key>
+      <string>com.runlevel1.lo0.192.168.237.237</string>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>ProgramArguments</key>
+      <array>
+          <string>/sbin/ifconfig</string>
+          <string>lo0</string>
+          <string>alias</string>
+          <string>192.168.237.237</string>
+      </array>
+      <key>StandardErrorPath</key>
+      <string>/var/log/loopback-alias.log</string>
+      <key>StandardOutPath</key>
+      <string>/var/log/loopback-alias.log</string>
+  </dict>
+</plist>" | sudo tee -a /Library/LaunchDaemons/com.runlevel1.lo0.192.168.237.237.plist
+
+chmod 0644 /Library/LaunchDaemons/com.runlevel1.lo0.192.168.237.237.plist
+
+sudo chown root:wheel /Library/LaunchDaemons/com.runlevel1.lo0.192.168.237.237.plist
+
+sudo launchctl load /Library/LaunchDaemons/com.runlevel1.lo0.192.168.237.237.plist
--- setupdev.sh
+++ setupdev.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+if ! ifconfig lo0 | grep -q 192.168.237.237; then
+  echo "adding loopback alias"
+  ./addloopback.sh;
+fi
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR && git pull origin master
 
 docker ps -a | grep -v STATUS | awk '{print $2}' | sort | uniq | xargs -n 1 docker pull
+
+docker-compose -f devenv.yaml up -d
